33335f812b75dc047c458339962bc3c80f894ca8
Song Young-jin <myevan80@gmail.com>
2014-01-22 23:12:00 +0900

    + blog tempalte view

--- blog/views.py
+++ blog/views.py
@@ -4,19 +4,12 @@ from flask import Blueprint
 from flask import render_template
 
 
-blog_blueprint = Blueprint('blog', __name__, url_prefix='/blog')
+blog_blueprint = Blueprint('blog', __name__, url_prefix='/blog',  template_folder='templates')
 
 @blog_blueprint.route('/')
 @blog_blueprint.route('/index')
 def index():
     user = { 'nickname': 'Miguel' } # fake user
-    return '''
-<html>
-  <head>
-    <title>Home Page</title>
-  </head>
-  <body>
-    <h1>Hello, ''' + user['nickname'] + '''</h1>
-  </body>
-</html>
-'''
+    return render_template("index.html",
+        title = 'Home',
+        user = user)
