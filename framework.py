e620d80e6d2650a56574485b4adee4bd75e01c57
Song Young-jin <myevan80@gmail.com>
2014-01-22 23:01:13 +0900

    + 프레임워크 디비

--- framework.py
+++ framework.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask
 from environments import Environments
+from database import SQLAlchemy
 
 app = Flask(__name__)
 env = Environments(app, os.path.dirname(os.path.realpath(__file__)))
+db = SQLAlchemy(app)
--- manage.py
+++ manage.py
@@ -5,15 +5,24 @@ import argparse
 
 from framework import app, env
 
-def run_shell(command_namespace):
+def run_shell(ns):
     code.interact('SHELL', local=dict(app=app, env=env))
+
+def run_server(ns):
+    app.run('0.0.0.0', port=ns.port)
 
 def main(program_path, program_args):
     main_parser = argparse.ArgumentParser()
     sub_parsers = main_parser.add_subparsers()
+
+    server_parser = sub_parsers.add_parser('server')
+    server_parser.add_argument('--port', type=int, default=5000, help='port') 
+    server_parser.set_defaults(func=run_server)
+
     shell_parser = sub_parsers.add_parser('shell')
     #shell_parser.add_argument('--config', action='store', help='config path') 
     shell_parser.set_defaults(func=run_shell)
+
     if program_args is None or len(program_args) == 0:
         main_parser.print_help()
         return -1
