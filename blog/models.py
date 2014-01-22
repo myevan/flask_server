8dd7c0ab3194715e3a33fa49f761d9bcd3dbbd0e
Song Young-jin <myevan80@gmail.com>
2014-01-23 00:14:45 +0900

    + blog models

---
+++ blog/models.py
@@ -0,0 +1,31 @@
+from framework import db
+
+ROLE_USER = 0
+ROLE_ADMIN = 1
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nickname = db.Column(db.String(64), unique = True)
+    email = db.Column(db.String(120), unique = True)
+    role = db.Column(db.SmallInteger, default = ROLE_USER)
+    posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
+
+if __name__ == '__main__':
+    db.drop_all()
+    db.create_all()
+
+    u = User(nickname='a', email='a@example.com', role=0)
+    db.session.add(u)
+    db.session.commit()
