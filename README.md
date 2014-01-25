b103696c7169b21e7eba2041890d4e08cf35f22e
Song Young-jin <myevan80@gmail.com>
2014-01-26 04:21:05 +0900

    ! 관리 스크립트 정리

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-flask_server
+Flask Server
 ============
 
 python flask server
+
+Virtual Environment
+-------------------
+
+#### Setup
+
+    $ cd ~/PROJECTS/flask_server
+    $ mkdir -p venv
+    $ virtualenv venv/flask_server
+    $ 
--- manage.py
+++ manage.py
@@ -1,20 +1,64 @@
 #!/usr/bin/env python
+# -*- coding:utf8 -*-
 import os
+import sys
 import code
 import argparse
 
-import blog
+def exec_command_line(exe_path, args):
+    cmd_line = '%s %s' % (exe_path, ' '.join(args))
+    print cmd_line
+    return os.system(cmd_line)
 
-from framework import app, env, db
+try:
+    import blog
+
+    from framework import app, env, db
+
+except ImportError as e:
+    venv_dir_path = os.environ.get('VIRTUAL_ENV', None)
+    if venv_dir_path is None:
+        print(e)
+        print('')
+
+        workon_home_dir_path = os.environ.get('WORKON_HOME', None)
+        if workon_home_dir_path is None:
+            print('\t$ sudo pip install virtualenvwrapper')
+            print('')
+            print('\t$ vim ~/.bash_profile')
+            print('\texport WORKON_HOME=~/VIRTUAL_ENVIRONEMNT_ROOT')
+            print('\tsource /usr/local/bin/virtualenvwrapper.sh')
+            sys.exit(-1)
+        else:
+            print('make virtual environment')
+            print('\t$ mkvirtualenv [VIRTUAL_ENV_NAME]')
+            print('')
+            print('or work on virtual environment')
+            print('\t$ workon [VIRTUAL_ENV_NAME]')
+            sys.exit(-2)
+    else:
+        print('install requirements')
+        print('\t$ pip install -r requirements.txt')
+
+def install_package(ns):
+    if not ns.package_names:
+        print 'NO_SOURCE_PATH'
+        return -101
+
+    for package_name in ns.package_names:
+        exec_command_line(EXE_VENV_PIP, ['install', package_name])
+
+    exec_command_line(EXE_VENV_PIP, ['freeze', '> requirements.txt'])
 
 def run_script(ns):
-    if not ns.source_path:
+    if not ns.source_paths:
         print 'NO_SOURCE_PATH'
         return -101
 
     env.load_config_file('./config/flask/blog.yml')
 
-    execfile(ns.source_path, globals())
+    for source_path in ns.source_paths:
+        execfile(ns.source_path, globals())
 
 def run_shell(ns):
     env.load_config_file('./config/flask/blog.yml')
@@ -30,21 +74,26 @@ def run_server(ns):
     app.register_blueprint(blog.bp)
     app.run('0.0.0.0', port=ns.port)
 
+
 def main(program_path, program_args):
     main_parser = argparse.ArgumentParser()
     sub_parsers = main_parser.add_subparsers()
 
-    server_parser = sub_parsers.add_parser('server')
-    server_parser.add_argument('-P', '--port', type=int, default=5000, help='port') 
-    server_parser.set_defaults(func=run_server)
+    install_package_parser = sub_parsers.add_parser('install_package')
+    install_package_parser.add_argument('package_names', type=str, nargs='+', help='package names')
+    install_package_parser.set_defaults(func=install_package)
 
-    script_parser = sub_parsers.add_parser('script')
-    script_parser.add_argument('-S', '--source-path', type=str, help='python source path') 
-    script_parser.set_defaults(func=run_script)
+    run_script_parser = sub_parsers.add_parser('run_script')
+    run_script_parser.add_argument('source_paths', type=str, nargs='+', help='python source path') 
+    run_script_parser.set_defaults(func=run_script)
 
-    shell_parser = sub_parsers.add_parser('shell')
-    #shell_parser.add_argument('--config', action='store', help='config path') 
-    shell_parser.set_defaults(func=run_shell)
+    run_server_parser = sub_parsers.add_parser('run_server')
+    run_server_parser.add_argument('-P', '--port', type=int, default=5000, help='port') 
+    run_server_parser.set_defaults(func=run_server)
+
+
+    run_shell_parser = sub_parsers.add_parser('run_shell')
+    run_shell_parser.set_defaults(func=run_shell)
 
     if program_args is None or len(program_args) == 0:
         main_parser.print_help()
