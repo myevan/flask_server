ea2bd7b01740cf69cae908f73a13ad509e1160d8
Song Young-jin <myevan80@gmail.com>
2014-01-26 05:15:36 +0900

    ! 데이터베이스 바인딩 코드 개선

--- server/database.py
+++ server/database.py
@@ -1,8 +1,29 @@
+# -*- coding:utf8 -*-
+import re
+
 from flask_sqlalchemy import SQLAlchemy as BaseSQLAlchemy
 from flask_sqlalchemy import _SignallingSession as BaseSignallingSession
 from flask_sqlalchemy import orm, partial, get_state
 
-import re
+class _BindingKeyPattern(object):
+    def __init__(self, db, pattern):
+        self.db = db 
+        self.raw_pattern = pattern
+        self.compiled_pattern = re.compile(pattern)
+        self._shard_keys = None
+
+    def __repr__(self):
+        return "%s<%s>" % (self.__class__.__name__, self.raw_pattern)
+
+    def match(self, key):
+        return self.compiled_pattern.match(key)
+
+    def get_shard_key(self, hash_num):
+        if self._shard_keys is None:
+            self._shard_keys = [key for key, value in self.db.app.config['SQLALCHEMY_BINDS'].iteritems() if self.compiled_pattern.match(key)]
+            self._shard_keys.sort()
+
+        return self._shard_keys[hash_num % len(self._shard_keys)]
 
 
 class _BoundSection(object):
@@ -32,28 +53,43 @@ class _SignallingSession(BaseSignallingSession):
         self._binding_key = self._binding_keys.pop()
 
     def get_bind(self, mapper, clause=None):
-        if mapper is None:
-            binding_key = self._binding_key
-        else:
-            if self._binding_key is None:
-                info = getattr(mapper.mapped_table, 'info', {})
-                binding_key = info.get('bind_key')
-            else:
-                binding_key = self._binding_key
-
+        binding_key = self.__find_binding_key(mapper)
         if binding_key is None:
             return BaseSignallingSession.get_bind(self, mapper, clause)
         else:
             state = get_state(self.app)
             return state.db.get_engine(self.app, bind=binding_key)
 
+    def __find_binding_key(self, mapper):
+        if mapper is None: # 맵퍼 없음
+            return self._binding_key
+        else:
+            mapper_info = getattr(mapper.mapped_table, 'info', {})
+            mapped_binding_key = mapper_info.get('bind_key')
+            if mapped_binding_key: # 맵핑된 바인딩 키 존재
+                if type(mapped_binding_key) is str: # 정적 바인딩
+                    return mapped_binding_key
+                else: # 동적 바인딩
+                    if mapped_binding_key.match(self._binding_key): # 현재 바인딩
+                        return self._binding_key
+                    else: # 푸쉬된 바인딩
+                        for pushed_binding_key in reversed(self._binding_keys):
+                            if pushed_binding_key and mapped_binding_key.match(pushed_binding_key):
+                                return pushed_binding_key
+                        else:
+                            raise Exception('NOT_FOUND_MAPPED_BINDING:%s CURRENT_BINDING:%s PUSHED_BINDINGS:%s' % (repr(mapped_binding_key), repr(self._binding_key), repr(self._binding_keys[1:])))
+            else: # 맵핑된 바인딩 키가 없으면 디폴트 바인딩
+                return self._binding_key
+
 
 class SQLAlchemy(BaseSQLAlchemy):
-    def bind(self, key):
+    def BindingKeyPattern(self, pattern):
+        return _BindingKeyPattern(self, pattern)
+
+    def binding(self, key):
         return _BoundSection(self.session, key)
 
     def create_scoped_session(self, options=None):
-        """Helper factory method that creates a scoped session."""
         if options is None:
             options = {}
         scopefunc=options.pop('scopefunc', None)
@@ -64,8 +100,6 @@ class SQLAlchemy(BaseSQLAlchemy):
     def get_binds(self, app=None):
         retval = BaseSQLAlchemy.get_binds(self, app)
        
-        # get the binds for None again in order to make sure that 
-        # it is the default bind for tables without an explicit bind 
         bind = None
         engine = self.get_engine(app, bind)
         tables = self.get_tables_for_bind(bind)
@@ -73,16 +107,17 @@ class SQLAlchemy(BaseSQLAlchemy):
         return retval
 
     def get_tables_for_bind(self, bind=None):
-        """
-        Returns a list of all tables relevant for a bind.
-        Tables without an explicit __bind_key__ will be bound to all binds.
-        """
         result = []
         for table in self.Model.metadata.tables.itervalues():
-            if bind and re.match(table.info.get('bind_key'), bind):
+            table_bind_key = table.info.get('bind_key')
+            if table_bind_key == bind:
                 result.append(table)
-            elif table.info.get('bind_key') is None:
-                result.append(table)
+            else:
+                if bind:
+                    if type(table_bind_key) is _BindingKeyPattern and table_bind_key.match(bind):
+                        result.append(table)
+                    elif type(table_bind_key) is str and table_bind_key == bind:
+                        result.append(table)
 
         return result
 
@@ -95,15 +130,15 @@ if __name__ == '__main__':
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
     app.config['SQLALCHEMY_BINDS'] = {
-        'data_1': 'sqlite:///./temp/test/data_1.db',
-        'data_2': 'sqlite:///./temp/test/data_2.db',
-        'log':  'sqlite:///./temp/test/log.db'
+        'data_1': 'sqlite:///../temp/test_data_1.db',
+        'data_2': 'sqlite:///../temp/test_data_2.db',
+        'log':  'sqlite:///../temp/test_log.db'
     }
 
     db = SQLAlchemy(app)
 
     class User(db.Model):
-        __bind_key__ = 'data_\d'
+        __bind_key__ = db.BindingKeyPattern('data_\d')
 
         id = db.Column(db.Integer, primary_key=True)
         nickname = db.Column(db.String(64), index=True, unique=True)
@@ -123,12 +158,12 @@ if __name__ == '__main__':
     db.drop_all()
     db.create_all()
 
-    with db.bind('data_1'):
+    with db.binding('data_1'):
         user = User(nickname='a')
         db.session.add(user)
         db.session.commit()
 
-    with db.bind('data_2'):
+    with db.binding('data_2'):
         user = User(nickname='b')
         db.session.add(user)
         db.session.commit()
@@ -137,9 +172,9 @@ if __name__ == '__main__':
     db.session.add(user_log)
     db.session.commit()
 
-    with db.bind('data_1'):
+    with db.binding('data_1'):
         print User.query.all()
 
-    with db.bind('data_2'):
+    with db.binding('data_2'):
         print User.query.all()
 
